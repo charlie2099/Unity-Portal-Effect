c3a4083d6459f0357a63be3d6b37c909805da2f8
Chilli01 <charlie6.evans@live.uwe.ac.uk>
2020-11-04 20:24:52 +0000

    implemented smooth portals

--- Portal Project/Assets/PortalCamera.cs
+++ Portal Project/Assets/PortalCamera.cs
@@ -11,6 +11,13 @@ public class PortalCamera : MonoBehaviour
     void Update()
     {
         Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+        //Vector3 playerOffsetFromPortal = new Vector3(playerCamera.transform.position.x, playerCamera.transform.position.y, playerCamera.transform.position.z + 2) - otherPortal.position;
         transform.position = portal.position + playerOffsetFromPortal;
+
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+        Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+        Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
     }
 }
